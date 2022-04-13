eda08536fa4812efe86719c3c17c56d47ef7dc79
darshan260802 <91478282+darshan260802@users.noreply.github.com>
2022-04-13 19:26:30 +0530

    Added option to translate title and description saperately

--- Routes/translate.js
+++ Routes/translate.js
@@ -4,15 +4,27 @@ const { translate } = require('bing-translate-api');
 
 router.post('/translate', async(req,res) => {
 
-    const {text, from , to } = req.body;
+    const {text, from , to, delimeter } = req.body;
 
+    if(delimeter)
+    {
+      const pt1 = await translate(text.split(delimeter)[0], from ?? null, to, true).then(res2 => res2.translation
+      ).catch(err => err);
+      // res.json({0:text.split(delimeter)[0], 1:text.split(delimeter)[1]})
 
-    await translate(text, from ?? null, to, true).then(res2 => {
-        res.status(200).json(res2)
-      }).catch(err => {
-        console.error(err);
-        res.status(500).json(err);
-      });
+      const pt2 = await translate(text.split(delimeter)[1], from ?? null, to, true).then(res2 => res2.translation).catch(err => err);
+
+      res.status(200).json({Title: pt1, Description: pt2});
+      
+    }else{
+      await translate(text, from ?? null, to, true).then(res2 => {
+          res.status(200).json(res2)
+        }).catch(err => {
+          console.error(err);
+          res.status(500).json(err);
+        });
+    }
+
 })
 
 module.exports = router;
